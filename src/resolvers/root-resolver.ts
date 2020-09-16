038049a4d6f00267a9e4237060b4d02586d7c9d8
Robert Sell <3444545+rnsell@users.noreply.github.com>
2020-09-16 19:03:25 -0400

    feat: add resolvers for noble gas elements

--- src/resolvers/root-resolver.ts
+++ src/resolvers/root-resolver.ts
@@ -1,31 +1,40 @@
-import { Element } from "../models";
-import { GraphqlType } from "../typedefs";
-
-const elements: any[] = [
-  {
-    atomicNumber: 1,
-    name: "Hydrogen",
-    symbol: "H",
-  },
-  {
-    atomicNumber: 2,
-    name: "Helium",
-    symbol: "He",
-  },
-].map((element) => {
-  return new Element(element);
-});
+import { Element, convertDbArrayToElements } from "../graphql-models";
+import { getElements, getElementsByType, DB_ELEMENT_TYPES } from "../data";
 
 type Resolver<T> = (obj: T, context: any, info: any) => Promise<any> | any;
 
 export const resolvers = {
   Query: {
     hello: () => "Hello world!",
-    elements: () => elements,
+    elements: async () => {
+      const allElements = await getElements();
+      const boop = new Set();
+
+      const elements = convertDbArrayToElements(allElements);
+
+      return elements;
+    },
   },
   IAtomicElement: {
     __resolveType: (obj: any) => {
       return obj.__resolveType();
+    },
+  },
+  NobleGasElement: {
+    otherNobleGasElements: async (parent: Element) => {
+      const nobleGasDbElements = await getElementsByType(
+        DB_ELEMENT_TYPES.NOBLE_GAS
+      );
+      const nobleGasElements: Element[] = convertDbArrayToElements(
+        nobleGasDbElements
+      );
+      const nobleGasElementsWithoutCurrentElement = nobleGasElements.filter(
+        (element) => {
+          return element.atomicNumber !== parent.atomicNumber;
+        }
+      );
+
+      return nobleGasElementsWithoutCurrentElement;
     },
   },
 };
