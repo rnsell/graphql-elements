8b558e54b4c9e2d18374dbc7bf67ac8415a7aea0
Robert Sell <3444545+rnsell@users.noreply.github.com>
2020-09-16 18:57:45 -0400

    feat: create enum for the strings found in the database

---
+++ src/data/db-element-types.ts
@@ -0,0 +1,13 @@
+export enum DB_ELEMENT_TYPES {
+  NONMETAL = "Nonmetal",
+  NOBLE_GAS = "Noble Gas",
+  AlKALI_METAL = "Alkali Metal",
+  ALKALINE_EARTH_METAL = "Alkaline Earth Metal",
+  METALLOID = "Metalloid",
+  HALOGEN = "Halogen",
+  METAL = "Metal",
+  TRANSITION_METAL = "Transition Metal",
+  LANTHANIDE = "Lanthanide",
+  TRANSACTINIDE = "Transactinide",
+  NO_TYPE = "",
+}
